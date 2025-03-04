f424d0f8c52eb774a28eec04fa6a6d5d4252bc32
Fulail-kt <muhamedfulail77@gmail.com>
2025-03-05 05:19:57 +0530

    before-blast-01

--- server/dist/routers/user.router.js
+++ server/dist/routers/user.router.js
@@ -18,10 +18,15 @@ const eventModel_1 = __importDefault(require("../models/eventModel"));
 const prayerModel_1 = __importDefault(require("../models/prayerModel"));
 const cloudinary_1 = require("cloudinary");
 /// cloudinary
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 cloudinary_1.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: 'dp19pgnnl',
+    api_key: '458556469471787',
+    api_secret: 'XruoucHbeb4XNtuPB-aXE5H8EX0',
 });
 exports.userRouter = (0, trpc_1.router)({
     getBloodDonors: trpc_1.privateProcedure
